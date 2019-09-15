ecdbd9c47560d8d5fba8b6392880639c6ce91fe2
Myriam Daniela Trevi.Abad <42685861+Myriam0213@users.noreply.github.com>
2019-09-14 22:07:00 -0500

    P3

--- Practica3.sql
+++ Practica3.sql
@@ -1,20 +1,27 @@
-alter table Name
+
+--RESTRICCIONES
+
+ALTER TABLE Name
 Add Constraint unique_username Unique(username)
 
 ALTER TABLE Identidad
 ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
 
 ALTER TABLE Persona
-ADD CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Sandnes');
+ADD CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Monterrey');
+
+ALTER TABLE Persons
+ADD CHECK (P_Id>0)
+
+ALTER TABLE proyectoFechaInicio
+ADD CONSTRAINT p_fch_ck CHECK (proyectoFechaInicio<=to_date('2009-09-16','YYYY-MM-DD'))
 
 
 
 
+--LLAVES FORANEAS
 
-
-
-
-ALTER TABLE nombreEsquema.nombreTabla
+ALTER TABLE NombreC.Cliente
 ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
 REFERENCES nombreEsquema.otraTabla (nombreColumna1)
 
