9a2c30cf37be45995be6391af86434e83412233d
Myriam Daniela Trevi.Abad <42685861+Myriam0213@users.noreply.github.com>
2019-09-20 23:34:53 -0500

    Update Practica3.sql

--- Practica3.sql
+++ Practica3.sql
@@ -1,45 +1,83 @@
+create database AdoptaPets
+use AdoptaPets
 
---RESTRICCIONES
+--Tabla Persona
 
-ALTER TABLE Name
-Add Constraint unique_username Unique(username)
+create table Persona( 
+	Persona_id int not null,
+	Nom_p varchar(150) not null,
+	Edad_p TINYINT CHECK (Edad_p>18) not null,
+	Telefono_p bigint not null,
+	Correo_p VARCHAR(100) unique,
+	INE bit not null CHECK(Si tiene INE=1 or No tiene INE=0),
+	ComprobanteDomicilio bit not null,
 
-ALTER TABLE Identidad
-ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
+	constraint pk_pi primary key(Persona_id),
+	--constraint pk_Np primary key(Nom_p)
+)
 
-ALTER TABLE Persona
-ADD CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Monterrey');
+--Tabla Mascota
 
-ALTER TABLE Persons
-ADD CHECK (P_Id>0)
+create table Mascota( 
+	Mascota_id int not null,
+	Años tinyint not null,
+	Raza varchar(80) not null,
+	Color varchar(30) not null,
+	Tamaño varchar(30) not null, 
+	
+	constraint pk_mi primary key(Mascota_id)
+)
 
-ALTER TABLE proyectoFechaInicio
-ADD CONSTRAINT p_fch_ck CHECK (proyectoFechaInicio<=to_date('2009-09-16','YYYY-MM-DD'))
+--Tabla ContratoAdopcion
+
+create table ContratoAdopcion( 
+	ContratoAdopcion_id int not null,
+	Persona_id2 int not null,
+	Mascota_id2 int not null,
+	Nom_p2 varchar(150) not null,
+	CostoCubrir float not null,
+	FechaAdopcion date not null,
+	
+	constraint pk_CAi primary key(ContratoAdopcion_id),
+	constraint fk_Pi2 foreign key(Persona_id2) references Persona(Persona_id),
+	constraint fk_Mi2 foreign key(Mascota_id2) references Mascota(Mascota_id),
+	--constraint fk_Np2 foreign key(Nom_p2) references Persona(Nom_p)
+)
+
+--Tabla CartillaVacunacion
+
+create table CartillaVacunacion( 
+	CartillaVacunacion_id int not null,
+	Persona_id3 int not null,
+	Mascota_id3 int not null,
+	Vacunas varchar(250) not null,
+	CostoVacunas float not null,
+	FechaVacunas date not null,
+
+	constraint pk_cvi primary key(CartillaVacunacion_id),
+	constraint fk_Pi3 foreign key(Persona_id3) references Persona(Persona_id),
+	constraint fk_Mi3 foreign key(Mascota_id3) references Mascota(Mascota_id)
+)
+
+--Tabla EntregaDeMascota
+
+create table EntregaDeMascota(
+	EntregaDeMascota_id int not null,
+	Mascota_id4 int not null,
+	Persona_id4 int not null,
+	CartillaVacunacion_id2 int not null,
+	ContratoAdopcion_id2 int not null,
+	
+	constraint pk_edmi primary key(EntregaDeMascota_id),
+	constraint fk_Pi4 foreign key(Persona_id4) references Persona(Persona_id),
+	constraint fk_CAi2 foreign key(ContratoAdopcion_id2) references ContratoAdopcion(ContratoAdopcion_id),
+	constraint fk_CVi2 foreign key(CartillaVacunacion_id2) references CartillaVacunacion(CartillaVacunacion_id),
+	constraint fk_Mi4 foreign key(Mascota_id4) references Mascota(Mascota_id)
+)
 
 
-
-
---LLAVES FORANEAS
-
-ALTER TABLE NombreC.Cliente
-ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
-REFERENCES nombreEsquema.otraTabla (nombreColumna1)
-                                                            
-ALTER TABLE NombreC.Cliente
-ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
-REFERENCES nombreEsquema.otraTabla (nombreColumna1
-
-ALTER TABLE NombreC.Cliente
-ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
-REFERENCES nombreEsquema.otraTabla (nombreColumna1)
-                                    
-ALTER TABLE NombreC.Cliente
-ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
-REFERENCES nombreEsquema.otraTabla (nombreColumna1)
-                                    
-ALTER TABLE NombreC.Cliente
-ADD CONSTRAINT FK_nombreRestriccion FOREIGN KEY(nombreColumna1)
-REFERENCES nombreEsquema.otraTabla (nombreColumna1)
-
-
-
+select * from Persona
+select * from Mascota 
+select * from EntregaDeMascota
+select * from ContratoAdopcion
+select * from CartillaVacunacion
